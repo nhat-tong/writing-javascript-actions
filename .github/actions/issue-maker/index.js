2b6aa800cb668517f2311e2588faa266119e40f1
nhat_hoang_tong <nhat_hoang_tong@carrefour.com>
2021-08-13 22:55:38 +0200

    update 2

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -11,14 +11,14 @@ async function run() {
         console.log(`joke: ${jokeBody}`);
 
         const octokit = github.getOctokit(token);
-        console.log(octokit.OctokitWithDefaults);
+        console.log(octokit.issues);
 
-        const newIssue = await octokit.issues.create({
-            repo: github.context.repo.repo,
-            owner: github.context.repo.owner,
-            title: issueTitle,
-            body: jokeBody
-        });
+        // const newIssue = await octokit.issues.create({
+        //     repo: github.context.repo.repo,
+        //     owner: github.context.repo.owner,
+        //     title: issueTitle,
+        //     body: jokeBody
+        // });
     } catch (err) {
         core.setFailed(err.message);
     }
