518d15f282b2adbf56307db72e1a4dbb12465a8c
nhat_hoang_tong <nhat_hoang_tong@carrefour.com>
2021-08-13 23:14:18 +0200

    fix octokit call

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,18 +7,14 @@ async function run() {
         const jokeBody = core.getInput("joke");
         const token = core.getInput("repo-token");
 
-        console.log(`mytoken: ${token}`);
-        console.log(`joke: ${jokeBody}`);
-
         const octokit = github.getOctokit(token);
-        console.log(octokit.defaults.issues);
 
-        // const newIssue = await octokit.issues.create({
-        //     repo: github.context.repo.repo,
-        //     owner: github.context.repo.owner,
-        //     title: issueTitle,
-        //     body: jokeBody
-        // });
+        const newIssue = await octokit.rest.issues.create({
+            repo: github.context.repo.repo,
+            owner: github.context.repo.owner,
+            title: issueTitle,
+            body: jokeBody
+        });
     } catch (err) {
         core.setFailed(err.message);
     }
